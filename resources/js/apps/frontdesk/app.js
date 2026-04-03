29ab233564e070f0a049220c1e28df3ba7524aed
necrom9608 <david@primecore.be>
2026-04-03 10:14:08 +0200

    INIT

--- resources/js/apps/frontdesk/app.js
+++ resources/js/apps/frontdesk/app.js
@@ -4,15 +4,6 @@ import axios from '@/lib/http'
 import router from './router'
 import FrontdeskApp from './App.vue'
 
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-axios.defaults.withCredentials = true
-axios.defaults.withXSRFToken = true
-
-const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
-if (csrfToken) {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
-}
-
 axios.interceptors.response.use(
     response => response,
     error => {
